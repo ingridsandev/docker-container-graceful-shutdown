7106377a728d734e425a94d9ecfee56d4ce4894d
Ingrid Santos <ingrid.dossantos@emailage.com>
2021-06-10 17:57:28 +0100

    docker container graceful shutdown

---
+++ src/Clean.Shut.Down.Web.Api/ApplicationLifetimeHostedService.cs
@@ -0,0 +1,64 @@
+using Microsoft.Extensions.Hosting;  
+using Microsoft.Extensions.Logging;  
+using System.Threading;  
+using System.Threading.Tasks;
+
+namespace Clean.Shut.Down.Web.Api
+{
+    public class ApplicationLifetimeHostedService : IHostedService  
+        {
+            private readonly IApplicationLifetime _applicationLifetime;
+            private readonly ILogger<ApplicationLifetimeHostedService> _logger;
+            public ApplicationLifetimeHostedService(
+                ILogger<ApplicationLifetimeHostedService> logger,   
+                IHostApplicationLifetime applicationLifetime)  
+            {
+                _logger = logger;  
+                _applicationLifetime = applicationLifetime;
+            }  
+  
+            public Task StartAsync(CancellationToken cancellationToken)  
+            {  
+                _logger.LogInformation("StartAsync method called.");  
+  
+                _applicationLifetime.ApplicationStarted.Register(() => OnStarted());  
+                _applicationLifetime.ApplicationStopping.Register(() => OnStopping());  
+                _applicationLifetime.ApplicationStopped.Register(() => OnStopped());
+                return Task.CompletedTask;
+            }  
+  
+            private void OnStarted()  
+            {  
+                _logger.LogInformation("DEBUG: Container has started.");
+            }  
+  
+            /// <summary>
+            /// Triggered when the application host is starting a graceful shutdown. Shutdown will block until all callbacks registered on this token have completed.
+            /// </summary>
+            private void OnStopping()  
+            {
+                // CancellationToken callback when receiving SIGTERM signal
+                
+                _logger.LogInformation("DEBUG: SIGTERM - Prep to stop container - CancellationToken callback");
+                
+                // Mocking time to do whatever
+                Thread.Sleep(1000);
+
+            }
+            
+            public Task StopAsync(CancellationToken cancellationToken)  
+            {  
+                _logger.LogInformation("DEBUG: Container will stop");
+                
+                return Task.CompletedTask;
+            }
+  
+            /// <summary>
+            /// Triggered when the application host has completed a graceful shutdown. The application will not exit until all callbacks registered on this token have completed.
+            /// </summary>
+            private void OnStopped()  
+            {  
+                _logger.LogInformation("DEBUG: SIGKILL - Container has stopped");
+            }
+        } 
+}
---
+++ src/Clean.Shut.Down.Web.Api/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Clean.Shut.Down.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddLogging();  
+            services.AddHostedService<ApplicationLifetimeHostedService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Clean.Shut.Down.Web.Api", Version = "v1"});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean.Shut.Down.Web.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
